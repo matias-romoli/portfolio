c8ab22c811242347715832b2b0e71784c2313f31
Matias <matiasromoli22@gmail.com>
2025-11-03 10:50:27 -0300

    changes newportfolio

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,15 @@ const $ = document.getElementById("header");
 $.addEventListener("scroll", () => {
   $.classList.toggle("scrolled", window.scrollY > 50);
 });
+
+const cards = document.querySelectorAll(".second-card.hidden");
+const btn = document.getElementById("btn");
+let expanded = false;
+
+btn.addEventListener("click", () => {
+  expanded = !expanded;
+  cards.forEach((card) => {
+    card.classList.toggle("hidden", !expanded);
+  });
+  btn.textContent = expanded ? "Ver menos" : "Ver más";
+});
